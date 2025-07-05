cdb5f7532de9c0723d191bb7bea03d068b7ebda7
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-05 15:04:03 +0700

    update project

---
+++ src/Week5/LoopWithJava/DrawGeometry.java
@@ -0,0 +1,51 @@
+package Week5.LoopWithJava;
+
+import java.util.Scanner;
+
+public class DrawGeometry {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Draw a triangle");
+            System.out.println("2. Draw a square");
+            System.out.println("3. Draw a rectangle");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw a triangle");
+                    System.out.println("******");
+                    System.out.println("*****");
+                    System.out.println("****");
+                    System.out.println("***");
+                    System.out.println("**");
+                    System.out.println("*");
+                    break;
+                case 2:
+                    System.out.println("Draw a square");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 3:
+                    System.out.println("Draw a rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice");
+            }
+        }
+    }
+}
