5f0bf559efa5c6c88f12bbb2b2c9525b702576ea
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-05 15:42:17 +0700

    update project

---
+++ src/Week5/LoopWithJava/IsPrime20Number.java
@@ -0,0 +1,38 @@
+package Week5.LoopWithJava;
+
+import java.util.Scanner;
+
+public class IsPrime20Number {
+    public static void main(String[] args) {
+        int count = 0;
+        int number;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number you want to check: ");
+        number = input.nextInt();
+
+        int N = 2;
+
+        System.out.print("The first integers is: ");
+        while (count < number) {
+            boolean isPrime = true;
+            if (N < 2) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i <= Math.sqrt(N); i++) {
+                    if (N % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(N + " ");
+                count++;
+            }
+
+            N++;
+        }
+    }
+}
