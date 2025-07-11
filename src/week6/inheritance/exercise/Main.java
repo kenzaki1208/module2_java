803cfc60131459eedc851efcbc553fc082ab890c
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-11 09:41:24 +0700

    update project

---
+++ src/week6/inheritance/exercise/Main.java
@@ -0,0 +1,23 @@
+package week6.inheritance.exercise;
+
+import week6.inheritance.exercise.ex1_1.Circle;
+import week6.inheritance.exercise.ex1_1.Cylinder;
+
+public class Main {
+    public static void main(String[] args) {
+//        Circle circle1 = new Circle();
+//        System.out.println(circle1);
+
+        Circle circle1 = new Circle(2.3, "red");
+        System.out.println(circle1);
+
+//        Cylinder cylinder1 = new Cylinder();
+//        System.out.println(cylinder1);
+
+        Cylinder cylinder = new Cylinder(2.3, 5.8, "orange");
+        System.out.println(cylinder);
+
+        Cylinder cylinder1 = new Cylinder(4.5, 7.6, "red");
+        System.out.println(cylinder1);
+    }
+}
