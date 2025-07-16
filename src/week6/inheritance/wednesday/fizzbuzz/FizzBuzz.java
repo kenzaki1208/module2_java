62738cc2b4ead51251e27910e78a2805a15a9d52
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-16 17:16:34 +0700

    update project

--- src/week6/inheritance/wednesday/fizzbuzz/FizzBuzz.java
+++ src/week6/inheritance/wednesday/fizzbuzz/FizzBuzz.java
@@ -4,11 +4,14 @@ public class FizzBuzz {
     public static String isFizzBuzz(int number) {
         boolean isCheckNumb3 = number % 3 == 0;
         boolean isCheckNumb5 = number % 5 == 0;
-        boolean checkNumb3 = String.valueOf(number).contains("3");
-        boolean checkNumb5 = String.valueOf(number).contains("5");
+//        boolean checkNumb3 = String.valueOf(number).contains("3");
+//        boolean checkNumb5 = String.valueOf(number).contains("5");
 
-        boolean isFizz = isCheckNumb3 || checkNumb3;
-        boolean isBuzz = isCheckNumb5 || checkNumb5;
+//        boolean isFizz = isCheckNumb3 || checkNumb3;
+//        boolean isBuzz = isCheckNumb5 || checkNumb5;
+
+        boolean isFizz = isCheckNumb3;
+        boolean isBuzz = isCheckNumb5;
 
         return getFizzBuzz(number, isFizz, isBuzz);
     }
