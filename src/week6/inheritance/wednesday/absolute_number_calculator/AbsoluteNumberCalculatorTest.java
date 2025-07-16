57f41a607f8d98ca74216a36b9df4b0d8389fe11
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-16 09:28:16 +0700

    update project

---
+++ src/week6/inheritance/wednesday/absolute_number_calculator/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,35 @@
+package week6.inheritance.wednesday.absolute_number_calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    void findAbsolute0() {
+        int number = 0;
+        int expected = 0;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findAbsolute1() {
+        int number = 1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void findAbsolute2() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+}
