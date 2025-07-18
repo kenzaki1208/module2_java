e6aafb9c17c927835483fa9015536c7738f6156b
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-18 09:03:34 +0700

    update project

---
+++ src/week6/inheritance/friday/practice/stack/MyStack.java
@@ -0,0 +1,45 @@
+package week6.inheritance.friday.practice.stack;
+
+public class MyStack {
+    private int arr[];
+    private int size;
+    private int index = 0;
+
+    public MyStack(int size) {
+        this.size = size;
+        arr = new int[size];
+    }
+
+    public void push(int element) {
+        if (isFull()) {
+            throw new StackOverflowError("Stack is full");
+        }
+        arr[index] = element;
+        index++;
+    }
+
+    public int pop() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Stack is empty");
+        }
+        return arr[--index];
+    }
+
+    public int size() {
+        return index;
+    }
+
+    public boolean isEmpty() {
+        if (index == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        if (index == size) {
+            return true;
+        }
+        return false;
+    }
+}
