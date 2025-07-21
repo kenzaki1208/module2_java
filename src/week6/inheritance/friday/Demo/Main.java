24a8ee58d0e005da9eee64a447c87fa89d6090d4
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-21 08:18:12 +0700

    update project

---
+++ src/week6/inheritance/friday/Demo/Main.java
@@ -0,0 +1,53 @@
+package week6.inheritance.friday.Demo;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//        Stack<String> history = new Stack<>();
+//        history.push("https://google.com");
+//        history.push("https://facebook.com");
+//        history.push("https://netflix.com");
+//
+//        System.out.println("Trang web đang truy cập: " + history.peek());
+//
+//        while (!history.isEmpty()) {
+//            String historyItem = history.pop();
+//            System.out.println(historyItem);
+//        }
+
+//        Queue<String> queue = new LinkedList<>();
+//        queue.add("Https://google.com");
+//        queue.add("Https://facebook.com");
+//        queue.add("Https://netflix.com");
+//
+//        System.out.println("Trang web đang truy cập: " + queue.peek());
+//
+//        while (!queue.isEmpty()) {
+//            String history =  queue.poll();
+//            System.out.println(history);
+//        }
+
+//        List<Student> students = new ArrayList<>();
+//        students.add(new Student("Wanbi", 3.4));
+//        students.add(new Student("Vinh", 3.9));
+//        students.add(new Student("Quân", 3.6));
+//
+//        for (Student student : students) {
+//            System.out.println(student);
+//        }
+
+//        PriorityQueue<Student> students = new PriorityQueue<>();
+//        students.add(new Student("Wanbi", 3.4));
+//        students.add(new Student("Vinh", 3.9));
+//        students.add(new Student("Quân", 3.6));
+//
+//        for (Student stud : students) {
+//            System.out.println(stud);
+//        }
+
+
+
+
+    }
+}
