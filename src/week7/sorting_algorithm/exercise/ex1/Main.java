13cde9a88f9a9191b967c316bc12e8979c9cf0ab
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-23 09:17:36 +0700

    update project

---
+++ src/week7/sorting_algorithm/exercise/ex1/Main.java
@@ -0,0 +1,14 @@
+package week7.sorting_algorithm.exercise.ex1;
+
+import static week7.sorting_algorithm.exercise.ex1.SelectionSort.selectionSort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] list = {1, 9, 4, 6, 5, -4, 12, -7};
+        selectionSort(list);
+//        for (int v : list) {
+//            System.out.print(v + " ");
+//        }
+//        System.out.println();
+    }
+}
