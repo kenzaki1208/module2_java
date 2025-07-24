f0dae1f0c7de68a68568322ced656f79173df667
kenzaki1208 <duytrinh25072001@gmail.com>
2025-07-24 09:16:41 +0700

    update project

---
+++ src/week7/Debug/practice/p1/ArrayExample.java
@@ -0,0 +1,16 @@
+package week7.Debug.practice.p1;
+
+import java.util.Random;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random random = new Random();
+        Integer[] array = new Integer[100];
+        System.out.println("Danh sách phần tử của mảng: " );
+        for (int i = 0; i < 100; i++) {
+            array[i] = random.nextInt(100);
+            System.out.print(array[i] + " ");
+        }
+        return array;
+    }
+}
---
+++ src/week7/Debug/practice/p1/Main.java
@@ -0,0 +1,19 @@
+package week7.Debug.practice.p1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] array = arrayExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của 1 phần tử bất kỳ: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + array[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
